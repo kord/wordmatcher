1c65ff6131c5d595b2fcd52a82bd9ed59620b746
Dave Kordalewski <davekordalewski@gmail.com>
2024-01-11 18:56:09 +0800

    Add matcherGame.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 import './dictionaries/mandarin';
+import MatcherGame from "./components/matcherGame";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Wordmatcher
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+
+                <MatcherGame/>
+                
             </header>
         </div>
     );
---
+++ src/components/matcherGame.tsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class MatcherGame extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    Matcher Game!
+                </p>
+            </div>
+        );
+    }
+}
+
+
+export default MatcherGame;

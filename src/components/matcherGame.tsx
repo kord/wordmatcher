354145ea00c1edcb6f417147a6372d8098abff3a
Dave Kordalewski <davekordalewski@gmail.com>
2024-01-12 18:00:22 +0800

    Game component building

--- src/components/matcherGame.tsx
+++ src/components/matcherGame.tsx
@@ -1,16 +1,60 @@
 import React, {Component} from 'react';
-import '../css/matcherGame.css';
+import '../css/matcherGame.scss';
 
-class MatcherGame extends Component {
+interface MatcherGameProps {
+}
+
+interface MatcherGameState {
+    gameActive: boolean,
+}
+
+class MatcherGame extends Component<MatcherGameProps, MatcherGameState> {
+    constructor(props: MatcherGameProps) {
+        super(props);
+        this.state = {
+            gameActive: false,
+        };
+    }
+
     render() {
         return (
             <div className={'matcher-game'}>
                 <div className={'options-bar'}>
                     Options bar:
+                    <button>
+                        Options
+                    </button>
+                    <button>
+                        Options2
+                    </button>
+                    <button>
+                        Options3
+                    </button>
                 </div>
-                <p>
-                    Matcher Game!
-                </p>
+                <div className={'game-body'}>
+                    <p>
+                        Matcher Game!
+                    </p>
+
+                    <div className={'quiz-hint'}>爱</div>
+                    <div className={'quiz-response-panel'}>
+                        <div className={'quiz-response-grid'}>
+                            <div className={'quiz-response-option'}>
+                                1
+                            </div>
+                            <div className={'quiz-response-option'}>
+                                2
+                            </div>
+                            <div className={'quiz-response-option'}>
+                                3
+                            </div>
+                            <div className={'quiz-response-option'}>
+                                4
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         );
     }

a472b4b9d76dbaa83271eab577f98899006a7b0e
Dave Kordalewski <davekordalewski@gmail.com>
2024-01-13 08:56:51 +0800

    pointless initialization to show console log

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,30 @@ import logo from './logo.svg';
 import './css/App.css';
 
 import './dictionaries/mandarin';
+import {MatcherGameLogic} from "./logic/game";
 import MatcherGame from "./components/matcherGame";
 
+
+// Some initialization stuff to do before loading the app.
+const initialize = () => {
+    for (let i = 0; i < 4; i++) {
+        const m = new MatcherGameLogic();
+        const round = m.getNewRound(i);
+        console.log(round);
+        console.log("")
+    }
+}
+
 function App() {
+    initialize();
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
 
                 <MatcherGame/>
-                
+
+
             </header>
         </div>
     );

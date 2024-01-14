318338397064867ec831cded42a8ec9d3445f335
Dave Kordalewski <davekordalewski@gmail.com>
2024-01-14 11:51:52 +0800

    track wordlength in matcherDict.ts

--- src/dictionaries/matcherDict.ts
+++ src/dictionaries/matcherDict.ts
@@ -15,6 +15,9 @@ export class MatcherDict {
     l1word: Map<string, WordEntry>;
     l2word: Map<string, WordEntry>;
 
+    l1length: Map<number, WordEntry[]>;
+    l2length: Map<number, WordEntry[]>;
+
     constructor(entries: Array<[WordEntry, WordEntry]>) {
         console.assert(entries.length > 0, 'Cannot initialize dictionary with length 0 word list.');
         this.entries = entries;
@@ -23,10 +26,21 @@ export class MatcherDict {
 
         this.l1word = new Map();
         this.l2word = new Map();
+        this.l1length = new Map();
+        this.l2length = new Map();
 
         entries.forEach((entry, index) => {
             this.addPair(entry[0], entry[1]);
         });
+    }
+
+    private static insertLength(word: WordEntry, list: Map<number, WordEntry[]>) {
+        const length = word.word.length;
+        if (list.has(length)) {
+            list.get(length)!.push(word);
+        } else {
+            list.set(length, [word]);
+        }
     }
 
     addPair = (a: WordEntry, b: WordEntry) => {
@@ -34,11 +48,15 @@ export class MatcherDict {
             `Inserting wrong languages into dictionary.`);
         this.l1word.set(a.word, b);
         this.l2word.set(b.word, a);
+
+        MatcherDict.insertLength(a, this.l1length);
+        MatcherDict.insertLength(b, this.l2length);
     }
 
     random = () => {
         const index = Math.floor(Math.random() * this.entries.length);
         return this.entries[index];
     }
+
 }
 

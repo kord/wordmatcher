abfb4c28e6e3142c30c1a945a84f7f0c1cdecc83
Dave Kordalewski <davekordalewski@gmail.com>
2024-01-14 11:42:59 +0800

    nothing

--- src/dictionaries/traditionalSimplifiedConverter.ts
+++ src/dictionaries/traditionalSimplifiedConverter.ts
@@ -1,12 +1,13 @@
 import {Lang} from "./languages";
 import {WordEntry} from "./matcherDict";
+
 const OpenCC = require('opencc-js');
 
-const twConverter = OpenCC.Converter({ from: 'cn', to: 'tw' });
+const twConverter = OpenCC.Converter({from: 'cn', to: 'tw'});
 
 // Convert (default) Simplified words to their traditional counterparts.
 export const simplifiedToTwTraditional = (word: WordEntry) => {
-    console.assert(word.lang===Lang.ChineseSimplified);
+    console.assert(word.lang === Lang.ChineseSimplified);
     return {
         word: twConverter(word.word),
         lang: Lang.ChineseTraditional,

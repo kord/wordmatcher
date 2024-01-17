6920baefc82c6b50b9fce01c01ec997ea3fd3ee3
Dave Kordalewski <davekordalewski@gmail.com>
2024-01-17 18:30:49 +0800

    fix key error

--- src/components/matcherGame.tsx
+++ src/components/matcherGame.tsx
@@ -49,7 +49,11 @@ class MatcherGame extends Component<MatcherGameProps, MatcherGameState> {
                     <div className={'quiz-response-panel'}>
                         <div className={'quiz-response-grid'}>
                             {this.state.game.currentRound.options.map((option, i) =>
-                                <QuizResponseOption entry={option} onClick={this.selectOption} optionNumber={i}/>
+                                <QuizResponseOption
+                                    entry={option}
+                                    onClick={this.selectOption}
+                                    optionNumber={i}
+                                    key={i}/>
                             )}
 
                         </div>
